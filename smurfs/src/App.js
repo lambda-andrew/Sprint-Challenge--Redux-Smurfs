33a0f7ac73a0850be195ae062d9762c66d4ad35d
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-05-24 10:59:01 -0700

    Completed MVP, plus stretch, plus added dummy login function!

--- smurfs/src/App.js
+++ smurfs/src/App.js
@@ -10,11 +10,73 @@ import {connect} from 'react-redux';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  state = {
+    credentials: {
+      username: "",
+      password: "",
+    }
+  }
+
+  login = e => {
+    e.preventDefault();
+    const user = this.state.credentials.username
+    localStorage.setItem("user", user)
+    this.setState({
+      credentials: {
+        username: "",
+        password: "",
+      }
+    })
+  }
+
+  logout = e => {
+    e.preventDefault();
+    localStorage.removeItem("user")
+    this.setState({
+      credentials: {
+        username: "",
+        password: "",
+      }
+    }
+    )
+  }
+
+  handleChanges = e => {
+    this.setState({
+        credentials: {
+            ...this.state.credentials,
+            [e.target.name]: e.target.value
+        }
+    })
+}
+
   render() {
     console.log("If there's an error, it'll show here:", this.props.error)
+    if (!localStorage.getItem("user")) {
+      return (
+        <div>
+          <form onSubmit={this.login}>
+            <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={this.handleChanges}
+            />
+            <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={this.handleChanges}
+            />
+            <button type="submit">Click to Log In!</button>
+          </form>
+        </div>
+      )
+    }
     return (
       <div className="App">
         <h1>This is the Village!</h1>
+        <button onClick={this.logout}>Click to Log Out!</button><br></br>
         <AddSmurfForm />
         <SmurfList />
       </div>
