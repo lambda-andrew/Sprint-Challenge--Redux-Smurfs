09ae0c5be1b4407ce4ab8372357e3b4df8ffcb48
Sydney Crumley <srsimps19@gmail.com>
2019-05-24 11:07:03 -0500

    built out smurfs list

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { getSmurfs, addSmurf } from '../actions'
+
+class SmurfList extends React.Component {
+    componentDidMount() {
+        this.props.getSmurfs()
+    }
+
+    render() {
+        if (this.props.fetchingSmurfs)
+            return(
+                <div className='fetchingSmurfsDiv'>
+                    <h2>Please Wait While We Find The Smurfs....</h2>
+                </div>
+            )
+        return (
+            <div className='smurfsListDiv'>
+                <h2>Welcome to Smurf Village!</h2>
+                <h3>Meet The Smurfs: </h3>
+                {this.props.smurfs.map(smurf => {
+                    return (
+                        <div className="SmurfCard">
+                            <h4>{smurf.name}</h4>
+                            <p>{smurf.age}</p>
+                            <p>{smurf.height}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs
+  });
+
+  export default connect(mapStateToProps, { getSmurfs })(SmurfList)
