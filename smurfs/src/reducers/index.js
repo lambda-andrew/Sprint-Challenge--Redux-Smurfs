00109254b0fccb4b2e6b0d71d61754aca8e76298
Sydney Crumley <srsimps19@gmail.com>
2019-05-24 11:53:40 -0500

    added delete to reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -5,6 +5,8 @@ import {
   ADD_SMURF_START,
   ADD_SMURF_SUCCESS,
   ADD_SMURF_FAIL,
+  DELETE_SMURF_START,
+  DELETE_SMURF_SUCCESS,
 } from '../actions'
 
 
@@ -12,7 +14,8 @@ import {
    smurfs: [],
    fetchingSmurfs: false,
    addingSmurf: false,
-   error: null
+   error: null,
+   deletingSmurf: false
  }
 
 const reducer = (state = initialState, action) => {
@@ -52,6 +55,18 @@ const reducer = (state = initialState, action) => {
         ...state,
         addingSmurf: false,
         error: action.payload
+      }
+    case DELETE_SMURF_START:
+      return {
+        ...state,
+        deletingSmurf: true
+      }
+    case DELETE_SMURF_SUCCESS:
+      return {
+        ...state,
+        deletingSmurf: false,
+        error: '',
+        smurfs: action.payload
       }
     default:
       return state
