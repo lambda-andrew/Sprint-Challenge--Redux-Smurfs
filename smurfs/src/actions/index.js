8f1126708adc097cedfad42de100e939c4bf4fbf
Sydney Crumley <srsimps19@gmail.com>
2019-05-24 10:41:45 -0500

    built out actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,38 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios'
+
+export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
+export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL';
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCH_SMURFS_START})
+  axios 
+    .get('http://localhost:3333/smurfs')
+    .then(res => {
+      dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data.smurfs })
+    })
+    .catch(err => {
+      console.log('Smurfs are hiding: ', err.response)
+      dispatch({ type: FETCH_SMURFS_FAIL, payload: err.response})
+    })
+}
+
+export const ADD_SMURF_START = 'ADD_SMURF_START';
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
+export const ADD_SMURF_FAIL = 'ADD_SMURF_FAIL';
+
+export const addSmurf = smurf => dispatch => {
+  dispatch({ type: ADD_SMURF_START})
+  return axios
+    .post('http://localhost:3333/smurfs', smurf)
+    .then(res => {
+      dispatch({ type: ADD_SMURF_SUCCESS, payload: res.data})
+    })
+    .catch(err => {
+      console.log('Adding Smurf failed: ', err.response)
+      dispatch({ type: ADD_SMURF_FAIL, payload: err.response})
+    })
+}
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
