4d78d72f31aa574d5fa8b042c5099136ee4d0480
Sydney Crumley <srsimps19@gmail.com>
2019-05-24 10:53:47 -0500

    built out app.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+
+import SmurfList from './components/SmurfList';
+import SmurfForm from './components/SmurfForm';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
+     <Router>
+       <Link to='/'>Smurf Village</Link>
+       <Link to='/smurf-form'>Add A Smurf</Link>
+       <Route exact path='/' component={SmurfList} />
+       <Route path='/smurf-form' component={SmurfForm}/>
+     </Router>
     );
   }
 }
